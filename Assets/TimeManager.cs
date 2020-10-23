e3614e87e9fa783d8551458a0c9d17e6106e48e2
Cameron <cameron2.bell@live.uwe.ac.uk>
2020-10-23 02:29:07 +0100

    Added TimeScaling
    Does this want to be more jittery to give off a better feeling closer to what it might of been like on 2600?

---
+++ Assets/TimeManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager Instance { get; private set; }
+
+    [SerializeField]
+    private float currentPlayerSpeed;
+    [SerializeField]
+    private float currentGameSpeed;
+
+    private float minGameSpeed = 0.1f;
+    private float maxGameSpeed = 1f;
+
+
+    private GameObject player;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>().gameObject;
+        //Time.timeScale = gameSpeed;
+     
+    }
+
+    void Update()
+    {
+        currentPlayerSpeed = player.GetComponent<Rigidbody2D>().velocity.magnitude;
+        currentGameSpeed = Time.timeScale;
+        ChangeTime();
+    }
+
+    public void ChangeTime()
+    {
+        float timeSpeed = (currentPlayerSpeed >= 0.3f) ? maxGameSpeed: minGameSpeed;
+        Time.timeScale = Mathf.Lerp(Time.timeScale, timeSpeed, currentPlayerSpeed);
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+}
