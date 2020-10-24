ee07c9e93845e395c590ba48b00b0255bf2ada2e
Conrad Wilson <conradjwilson@hotmail.com>
2020-10-24 19:55:15 +0100

    Adding MainMenu, base OptionsMenu & Camera fix
    +Added Menu scene, made initial scene on game-start
    +Added logo & buttons to main menu
    +Added main menu button functionality
    +Added base options menu (back button only)
    *Fixed cross-scene camera scaling issues (though letter boxing on top and sides is a thing now)
    -Removed Jeff Cordon

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    CanvasManager canvasManager;
+
+    private void Start()
+    {
+        canvasManager = FindObjectOfType<CanvasManager>();
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void OpenMenu(string _type)
+    {
+        switch (_type)
+        {
+            case "OptionsMenu":
+                canvasManager.SwitchCanvas(CanvasType.OptionsMenu);
+                break;
+            case "MainMenu":
+                canvasManager.SwitchCanvas(CanvasType.MainMenu);
+                break;
+        }
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
